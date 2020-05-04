16cbe22ff58cc29cd8d765a9455827c680aee66d
Pedro Young <pedroyoung@livenation.com>
2020-05-03 23:24:54 -0700

    Add templates

--- application.py
+++ application.py
@@ -1,8 +1,9 @@
 import os
 
-from flask import Flask, session
-from flask_session import Session
-from sqlalchemy import create_engine
+import random
+from flask import Flask, session, render_template
+from flask_session import Session                
+from sqlalchemy import create_engine             
 from sqlalchemy.orm import scoped_session, sessionmaker
                                                       
 app = Flask(__name__)                                  
@@ -13,15 +14,15 @@ if not os.getenv("DATABASE_URL"):
                                                        
 # Configure session to use filesystem                  
 app.config["SESSION_PERMANENT"] = False                
-app.config["SESSION_TYPE"] = "filesystem"              
-Session(app)                                           
-                                                       
-# Set up database                                      
-engine = create_engine(os.getenv("DATABASE_URL"))      
-db = scoped_session(sessionmaker(bind=engine))         
-                                                       
-                                                       
-@app.route("/")                                        
-def index():                                           
-    return "Project 1: TODO"                           
-                                                       
+app.config["SESSION_TYPE"] = "filesystem"                               
+Session(app)                                                                   
+                                                                               
+# Set up database                                                              
+engine = create_engine(os.getenv("DATABASE_URL"))                              
+db = scoped_session(sessionmaker(bind=engine))                                 
+                                                                               
+                                                                               
+@app.route("/")                                                                
+def index():                                                                   
+    headline = random.choice(["Landing #1", "Landing #2", "Landing #3"])        
+    return render_template("index.html", headline=headline)                    
